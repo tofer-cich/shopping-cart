d8fc9ea59694ce3be18ecd9f001a3c0b6e46ad55
Tofer-Cich <[tofercich@hotmail.com]>
2025-05-08 16:52:56 -0500

    Create navBar
    Site needs a way to be able to route back to desired pages

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,18 @@
 function HomePage() {
-    return;
+
+    // This is all just for testing the navBar component
+    // Please delete this
+    let myArr = [];
+
+    for (let i = 0; i < 200; i++) {
+        myArr[i] = i;
+    }
+
+    return (
+        <div id="home-page">
+            {myArr.map((i) => <p>{i}</p>)}
+        </div>
+    );
 }
 
 export default HomePage;
